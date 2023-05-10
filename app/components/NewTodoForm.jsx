2b4bc38965aee34e5f2869e4ab2f840d698160bc
robfoss <robfoss@rocketmail.com>
2023-05-10 14:28:58 -0300

    started NewTodoForm component

--- app/components/NewTodoForm.jsx
+++ app/components/NewTodoForm.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { create } from '../_actions';
 
 const NewTodoForm = () => {
-  return <div>NewTodoForm</div>;
+  const formRef = useRef();
+
+  async function action(formData) {
+    await create(formData);
+    formRef.current.reset();
+  }
+
+  return (
+    <form ref={formRef} action={action}>
+      <h2 className='font-medium mb-2'>Create a New Todo</h2>
+      <input
+        type='text'
+        name='title'
+        className='border border-slate-400 px-2 py-0.5 rounded'
+      />
+      <button
+        type='submit'
+        className='ml-2 text-sm px-2 py-1 rounded bg-slate-700 text-white'
+      >
+        Add Todo
+      </button>
+    </form>
+  );
 };
 
 export default NewTodoForm;
